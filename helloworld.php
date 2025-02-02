77a424ca684023c594e16a054b8528f8a394a2a8
Matilde <tilde.heden@gmail.com>
2025-02-02 17:20:42 +0100

    Moment 1

---
+++ helloworld.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="sv">
+    <head>
+        <meta charset="utf-8"/>
+        <title>php20</title>
+    </head>
+    <body>
+        <h1>Mallsida i HTML</h1>
+        <?php echo "<p>Hello World from PHP.</p>"; ?>
+        <hr>
+    </body>
+</html>
